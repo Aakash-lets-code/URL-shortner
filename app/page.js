f5d4571f43b154e82a510875219d3bbac0a7c25c
Aakash lets code <akashshrivastav071@gmail.com>
2025-02-28 11:16:39 +0530

    start working on the main page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,21 @@
 import Image from "next/image";
 
 export default function Home() {
-  return ( 
-    <div>
-      I am main
-    </div>
+  return (
+    <main>
+      <section className="grid grid-cols-2 text-black h-[50vh] bg-purple-200 " >
+        <div className="flex flex-col items-center justify-center gap-2" >
+          <p className="text-xl font-bold " >
+            The best URL shortner in the market
+          </p>
+          <p>
+            We are the most straight forward URL shortner in the market.
+          </p>
+        </div>
+        <div className="flex justify-start relative " >
+          <Image alt="an image of the vector" src={"/vector.png"} fill={true} / >
+        </div>
+      </section>
+    </main>
   );
 }
