73bb1e7a0850eee9a71149eed8820955c735430f
Aakash lets code <akashshrivastav071@gmail.com>
2025-03-02 12:02:24 +0530

    design shorten page for now

--- app/shorten/page.js
+++ app/shorten/page.js
@@ -12,14 +12,16 @@ const shorten = () => {
     }
 
     return (
-        <div className=' mx-auto max-w-lg' >
+        <div className=' mx-auto max-w-lg flex flex-col my-16 p-4 rounded-lg bg-purple-300' >
 
-            <h1 className='py-5' >Genrate your short URLs</h1>
+            <h1 className='py-3 font-bold text-2xl text-center' >Genrate your short URLs</h1>
 
-            <div className='' >
-                <input className='p-4 py-2 focus:bg-purple-200 m-1' type='text' placeholder='Enter your long URL here' onChange={handlechange} />
-                <input className='p-4 py-2 focus:bg-purple-200 m-1' type="text" placeholder='Enter your preferred short URL' />
-                <button className='p-2 rounded-2xl bg-amber-800 text-white' >Generate</button>
+            <div className='flex flex-col gap-3 ' >
+                <input className='p-4 py-2 bg-white rounded-md focus:bg-purple-50000  ' type='text' placeholder='Enter your long URL here' onChange={handlechange} />
+                <input className='p-4 py-2 bg-white rounded-md focus:bg-purple-50000  ' type="text" placeholder='Enter your preferred short URL' onChange={handlechange} />
+                {/* <button className='p-2 rounded-2xl bg-amber-800 text-white' >Generate</button> */}
+                <button className='shadow-2xl px-3 py-1 rounded-xl bg-purple-600 font-bold text-white' >Generate</button>
+            {/* </div> */}
             </div>
 
         </div>
