cfd359d50216a38b53951ebf5608ebbe3ce035dc
Aakash lets code <akashshrivastav071@gmail.com>
2025-03-03 11:42:51 +0530

    make about and contact us page

--- component/Navbar.js
+++ component/Navbar.js
@@ -2,22 +2,28 @@ import React from 'react'
 import Link from 'next/link'
 
 const Navbar = () => {
+
   return (
-    <nav  className='h-14 bg-purple-800 flex justify-between items-center text-white px-3 py-2 '>
-        <div className='text-xl font-bold cursor-pointer ' >
-            BitLink
-        </div>
-        <ul className='flex justify-center gap-3 items-center' >
-           <Link href="/" > <li>Home</li> </Link>
-           <Link href="/about" > <li>About</li> </Link>
-           <Link href="/generate" > <li>Shorten</li> </Link>
-           <Link href="/contact" > <li>Contact Us</li> </Link>
-            <li className='gap-3' >
-                <Link href="/generate" > <button className='shadow-2xl px-3 py-1 rounded-xl bg-purple-600 font-bold ' >Try Now</button> </Link>
-                <Link href="/github" > <button className='shadow-2xl px-3 py-1 rounded-xl bg-purple-600 font-bold ' >Github</button> </Link>
-            </li>
-        </ul>
-        
+
+    <nav className='h-14 bg-purple-800 flex justify-between items-center text-white px-3 py-2 '>
+
+      <div className='text-xl font-bold cursor-pointer ' >
+        <Link href="/" >BitLink</Link>
+      </div>
+
+      <ul className='flex justify-center gap-3 items-center' >
+
+        <Link href="/" > <li>Home</li> </Link>
+        <Link href="/about" > <li>About</li> </Link>
+        <Link href="/shorten" > <li>Shorten</li> </Link>
+        <Link href="/contact" > <li>Contact Us</li> </Link>
+        <li className='gap-3' >
+          <Link href="/shorten" > <button className='shadow-2xl px-3 py-1 rounded-xl bg-purple-600 font-bold ' >Try Now</button> </Link>
+          <Link href="/github" > <button className='shadow-2xl px-3 py-1 rounded-xl bg-purple-600 font-bold ' >Github</button> </Link>
+        </li>
+
+      </ul>
+
     </nav>
   )
 }
