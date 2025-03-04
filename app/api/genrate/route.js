62fc6e962f748c846add86a3c676363896bccec6
Aakash lets code <akashshrivastav071@gmail.com>
2025-03-04 14:48:01 +0530

    create database and connect mongodb

--- app/api/genrate/route.js
+++ app/api/genrate/route.js
@@ -1,3 +1,20 @@
-export async function POST() {
+import clientPromise from "@/lib/mongodb"
+
+
+export async function POST(request) {
+
+    const body = request.json()
+    const client = await clientPromise;
+    const db = client.db("bitlinks")
+    const collection = db.collection("url")
+
+    // check if the short url exists
+
+    collection.insertOne({
+        url: body.url,
+        shorturl: body.shorturl
+    })
+
+
     return Response.json({ message: 'Hello World' })
   }
