d0eb57ca6fb44ad8a136c37578bc649a016d33f5
Aakash lets code <akashshrivastav071@gmail.com>
2025-03-05 12:07:55 +0530

    if short url already exist throw a error

--- app/api/genrate/route.js
+++ app/api/genrate/route.js
@@ -3,18 +3,22 @@ import clientPromise from "@/lib/mongodb"
 
 export async function POST(request) {
 
-    const body = request.json()
+    const body = await request.json()
     const client = await clientPromise;
     const db = client.db("bitlinks")
     const collection = db.collection("url")
 
     // check if the short url exists
+    const doc = await collection.findOne({ shorturl: body.shorturl })
+    if (doc) {
+        return Response.json({ success: false, error: true, message: 'Short URL already exists' })
+    }
 
-    collection.insertOne({
+    const result = await collection.insertOne({
         url: body.url,
         shorturl: body.shorturl
     })
 
 
-    return Response.json({ message: 'Hello World' })
-  }
+    return Response.json({ success: true, error: false, message: 'URL Genrated Successfully' })
+}
