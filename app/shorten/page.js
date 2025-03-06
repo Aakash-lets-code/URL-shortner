d7a5ed15e0bcc606a891308108361bf46ada0798
Aakash lets code <akashshrivastav071@gmail.com>
2025-03-06 10:11:04 +0530

    show the genrated url on screen

--- app/shorten/page.js
+++ app/shorten/page.js
@@ -1,20 +1,21 @@
 "use client";
 import React from 'react'
 import { useState } from 'react';
+import Link from 'next/link'
 
 const shorten = () => {
 
     const [url, seturl] = useState("");
     const [shorturl, setshorturl] = useState("");
-    const [generate, setGenerate] = useState(false);
+    const [generated, setGenerated] = useState("");
 
     const genrate = () => {
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
 
-        const raw = JSON.stringify({ 
-            "url": url, 
-            "shorturl": shorturl 
+        const raw = JSON.stringify({
+            "url": url,
+            "shorturl": shorturl
         });
 
         const requestOptions = {
@@ -27,10 +28,10 @@ const shorten = () => {
         fetch("/api/genrate", requestOptions)
             .then((response) => response.json())
             .then((result) => {
+                setGenerated(`${process.env.NEXT_PUBLIC_HOST}/${shorturl}`)
                 seturl("")
                 setshorturl("")
-                
-                console.log
+                console.log(result)
                 alert(result.message)
             })
             .catch((error) => console.error(error));
@@ -62,6 +63,13 @@ const shorten = () => {
                 />
                 {/* <button className='p-2 rounded-2xl bg-amber-800 text-white' >Generate</button> */}
                 <button onClick={genrate} className='shadow-2xl px-3 py-1 rounded-lg bg-purple-600 font-bold text-white' >Generate</button>
+
+                 <code className='w-full font-bold bg-purple-200 text-black text-center py-2 rounded-lg' >
+
+                   Your link is : <Link target="_blank" href={generated} >{generated}</Link> 
+
+                </code>
+
             </div>
 
         </div>
